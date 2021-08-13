26e2734a30e702bc1d02364b6cdf685c312d5eca
Anna Savchuk <anna060411@gmail.com>
2021-08-13 20:27:54 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import countriesApi from './js/fetchCountries';
+import countryTpl from './templates/country-card.hbs';
+import countriesTpl from './templates/country-card.hbs';
+import getRefs from './js/get-refs';
+
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+const refs = getRefs();
+const debounce = require('lodash.debounce');
+let foundedCountry = '';
+
+refs.input.addEventListener(
+  'input',
+  debounce(() => {
+    onSearch();
+  }, 500),
+);
+
+function onSearch() {
+  resetSearch();
+  foundedCountry = refs.input.value;
+  countriesApi(foundedCountry)
+    .then(renderMarkup)
+    .catch(err => console.log(err));
+}
+
+function resetSearch() {
+  refs.countriesContainer.innerHTML = '';
+}
+
+function renderMarkup(countries) {
+  if (countries.length === 1) {
+    resetSearch();
+    markupContries(countryTpl, countries);
+  } else if (countries.length > 1 && countries.length <= 10) {
+    resetSearch();
+    markupContries(countriesTpl, countries);
+  } else if (countries.length > 10) {
+    resultMessage(
+      error,
+      'To many matches found. Please enter a more specific query!',
+    );
+  } else {
+    resultMessage(info, 'No matches found!');
+  }
+}
+
+function resultMessage(typeInfo, textInfo) {
+  typeInfo({
+    text: `${textInfo}`,
+    delay: 1000,
+    closerHover: true,
+  });
+}
+
+function markupContries(tpl, countries) {
+  refs.countriesContainer.insertAdjacentHTML('beforeend', tpl(countries));
+}
