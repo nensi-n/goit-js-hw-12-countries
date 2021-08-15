3f1c2a650939025422b360da58942ee71b647789
Anna Savchuk <anna060411@gmail.com>
2021-08-15 22:19:11 +0300

    add css for background

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
 export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json());
+  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json()).catch(error => console.log(error));
 }
